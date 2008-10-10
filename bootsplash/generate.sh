ae21e868c8dd0bef64b56254cc0e641983b7e70c
baghira-style <baghira-style@d23db356-9f35-0410-88be-f4158e15d4e5>
2008-10-10 21:02:51 +0000

    new bootsplash to match kdm, ksplash to come
    git-svn-id: https://cloudcity.svn.sourceforge.net/svnroot/cloudcity@291 d23db356-9f35-0410-88be-f4158e15d4e5

--- bootsplash/generate.sh
+++ bootsplash/generate.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 if [ "$1" = "install" ]; then
-dest=${2:-"/etc/bootsplash/themes"}
-echo "Installed files will go to $dest/Bespin"
-echo -e "If you're asked for a password, it's the sudo one\n"
-sudo cp -rfd Bespin $dest/
-exit
+    dest=${2:-"/etc/bootsplash/themes"}
+    echo -e "\nInstalling files to $dest/Bespin\n
+    Please enter your sudo password:\n"
+    sudo cp -rfd Bespin $dest/
+    exit
 fi
 
 csize=$(fbresolution)
@@ -15,39 +15,46 @@ width=${size%x*}
 height=${size#*x}
 
 if [ ! $width ] || [ ! $height ]; then
-echo "usage: $0 WIDTHxHEIGHT"
-exit
-fi
-
-if [ "$(which inkscape 2>/dev/null )" = "" ]; then
-echo "Sorry, you need Inkscape to do this"
-exit
+    echo "usage: $0 <width>x<height>"
+    exit
 fi
 
 if [ "$(which convert 2>/dev/null )" = "" ]; then
-echo "Sorry, you need ImageMagick to do this"
-exit
+    echo "Sorry, you need ImageMagick to do this"
+    exit
+fi
+
+read -n1 -p "Do you have a widescreen (16:10) monitor? [y/N] " WIDESCREEN
+if [ "$WIDESCREEN" = "y" ] || [ "$WIDESCREEN" = "Y" ]; then
+    WIDESCREEN=true
+else
+    WIDESCREEN=false
 fi
 
 if [ ! -d Bespin ]; then mkdir Bespin; fi
 if [ ! -d Bespin/config ]; then mkdir Bespin/config; fi
 if [ ! -d Bespin/images ]; then mkdir Bespin/images; fi
 
-inkscape -e Bespin/images/bootsplash-${width}x${height}.png -w $width -h $height bootsplash.svg
-convert -quality 85 Bespin/images/bootsplash-${width}x${height}.png Bespin/images/bootsplash-${width}x${height}.jpg
-rm -f Bespin/images/bootsplash-${width}x${height}.png
-inkscape -e Bespin/images/silent-${width}x${height}.png -w $width -h $height silent.svg
-convert -quality 85 Bespin/images/silent-${width}x${height}.png Bespin/images/silent-${width}x${height}.jpg
-rm -f Bespin/images/silent-${width}x${height}.png
+if $WIDESCREEN; then
+    SILENT=silent-16210.jpg
+    SPLASH=bootsplash-16210.jpg
+else
+    SILENT=silent.jpg
+    SPLASH=bootsplash.jpg
+fi
 
-X1=$((472*$width/1280))
-Y1=$((618*$height/1024))
-Y2=$((626*$height/1024))
+convert -quality 85 -scale ${width}x${height} $SPLASH Bespin/images/bootsplash-${width}x${height}.jpg
+convert -quality 85 -scale ${width}x${height} $SILENT Bespin/images/silent-${width}x${height}.jpg
 
-TX=$((110*$width/1280))
-TY=$((50*$height/1024))
-TW=$((1140*$width/1280))
-TH=$((970*$height/1024))
+X2=$((1024*$width/1280))
+X3=$((512*$width/1280))
+Y1=$((918*$height/1024))
+Y2=$((926*$height/1024))
+
+TX=$((45*$width/1280))
+TY=0
+TW=$((1196*$width/1280))
+TH=$((846*$height/1024))
 
 echo -e "
 # This is a bootsplash configuration file for
@@ -80,9 +87,9 @@ trigger \"rlchange 6\" tosilent
 
 
 progress_enable=1
-box silent noover $X1 $Y1 $((800*$width/1280)) $Y2 #ffffff10
-box silent inter $X1 $Y1 $((480*$width/1280)) $Y2 #ffffff80
-box silent $X1 $Y1 $((800*$width/1280)) $Y2 #ffffff80
+box silent noover 0 $Y1 $X2 $Y2 #ffffff10
+box silent inter 0 $Y1 $X3 $Y2 #ffffff80
+box silent 0 $Y1 $X2 $Y2 #ffffff80
 
 " > Bespin/config/bootsplash-${width}x${height}.cfg
 
