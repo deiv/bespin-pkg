8ac47bc7abfd1a09ae4c9b6768c9fd8bd1f5665f
baghira-style <baghira-style@d23db356-9f35-0410-88be-f4158e15d4e5>
2009-08-27 22:08:08 +0000

    plasma propagates every widget as toplevel??? - block it from argb support, has internal argb anyway
    git-svn-id: https://cloudcity.svn.sourceforge.net/svnroot/cloudcity@560 d23db356-9f35-0410-88be-f4158e15d4e5

--- init.cpp
+++ init.cpp
@@ -231,7 +231,10 @@ Style::readSettings(const QSettings* settings, QString appName)
     config.bg.modal.invert = (appType != KDM) && readBool(BG_MODAL_INVERT);
     config.bg.intensity = clamp(100+readInt(BG_INTENSITY), 30, 300);
 #if BESPIN_ARGB_WINDOWS
-    config.bg.opacity = (appType == KWin ? 0xff : clamp(readInt(BG_OPACITY), 0, 0xff));
+    if (appType == KWin || appType == Plasma)
+        config.bg.opacity = 0xff;
+    else
+        config.bg.opacity = clamp(readInt(BG_OPACITY), 0, 0xff);
     if (config.bg.opacity != 0xff)
     {
         QStringList blacklist = iSettings->value(ARGB_BLACKLIST).toString().split(',', QString::SkipEmptyParts);
