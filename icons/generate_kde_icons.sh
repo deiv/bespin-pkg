fffb9dec74120a1e67a8a09b1c2a4aba78edcebc
baghira-style <baghira-style@d23db356-9f35-0410-88be-f4158e15d4e5>
2009-09-14 20:23:21 +0000

    import
    git-svn-id: https://cloudcity.svn.sourceforge.net/svnroot/cloudcity@599 d23db356-9f35-0410-88be-f4158e15d4e5

---
+++ icons/generate_kde_icons.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+setname="nmfnms"
+sizes="128 64 48 32 22 16"
+
+[ -d "$setname" ] || mkdir "$setname"
+for sz in $sizes; do
+    dir=$setname/${sz}x${sz}
+    [ -d "$dir" ] || mkdir "$dir"
+    for svg in *.svg; do
+        png="$dir/${svg%.svg}.png"
+        if [ ! -e $png ] || [ $svg -nt $png ]; then
+            inkscape -w $sz -e "$png" "$svg"
+        fi
+    done
+done
+
+    
+    
