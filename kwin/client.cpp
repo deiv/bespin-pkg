57ac2276ae8f87a971edb06fae510c047ddf3c20
baghira-style <baghira-style@d23db356-9f35-0410-88be-f4158e15d4e5>
2010-05-23 13:59:32 +0000

    revert experimental ARGB only rounding
    git-svn-id: https://cloudcity.svn.sourceforge.net/svnroot/cloudcity@1105 d23db356-9f35-0410-88be-f4158e15d4e5

--- kwin/client.cpp
+++ kwin/client.cpp
@@ -1247,7 +1247,7 @@ Client::resize( const QSize& s )
     left = QRect(0, t2, Factory::verticalTitle() ? myTitleSize : borderSize, sideHeight);
     right = QRect(w-borderSize, t2, borderSize, sideHeight);
 
-    if ( Factory::roundCorners() && !Factory::compositingActive() )
+    if ( Factory::roundCorners() /*&& !Factory::compositingActive()*/ )
     {
         if (maximizeMode() == MaximizeFull)
             { clearMask(); widget()->update(); return; }
