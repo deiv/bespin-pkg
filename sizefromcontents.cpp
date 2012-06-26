ecd12a91e87ab608363fe782e0446f454ee5a9fe
baghira-style <baghira-style@d23db356-9f35-0410-88be-f4158e15d4e5>
2012-06-26 19:13:55 +0000

    increase toolbuttonwidth for esp. slim (text-only) inlay buttons
    git-svn-id: https://cloudcity.svn.sourceforge.net/svnroot/cloudcity@1557 d23db356-9f35-0410-88be-f4158e15d4e5

--- sizefromcontents.cpp
+++ sizefromcontents.cpp
@@ -207,10 +207,14 @@ Style::sizeFromContents(ContentsType ct, const QStyleOption *option, const QSize
             h = contentsSize.height() + F(8);
         else
             h = contentsSize.height() + F(4);
-        if (config.btn.tool.connected && (config.btn.tool.frame == Raised || config.btn.tool.frame == Inlay) )
-            h += F(4);
 
-        w = qMax(contentsSize.width() + F(4), h*4/3); // 4/3 - 16/9
+        w = contentsSize.width() + F(4);
+        if (config.btn.tool.connected && (config.btn.tool.frame == Raised || config.btn.tool.frame == Inlay) ) {
+            h += F(4);
+            w += F(8);
+        }
+
+        qMax(w + F(4), h*4/3); // 4/3 - 16/9
 
         if (toolbutton && hasMenuIndicator(toolbutton))
             w += pixelMetric(PM_MenuButtonIndicator, option, widget)/* + F(4)*/;
